b1bf23f41f559b7b554834fea8ae568849e35515
satvik <sshukla9@cs.washington.edu>
2018-11-07 11:36:28 -0800

    added train test split

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 # %matplotlib inline
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 from data_processing import *
 
 DATA_PATH = '~/.kaggle/'
+
+def split_data(train_users):
+    train_users = train_users.sort_values(by='date_account_created')
+    new_train, new_test = train_test_split(train_users, test_size=0.3, shuffle=False)
+    return new_train, new_test
+
 
 # countries = pd.read_csv(DATA_PATH + 'countries.csv')
 # age_gender = pd.read_csv(DATA_PATH + 'age_gender_bkts.csv')
